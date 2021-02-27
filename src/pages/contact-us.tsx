6ccf151855782f924acd93f11b0c4c88db594070
Moritz Makowski <moritz@dostuffthatmatters.dev>
2021-02-27 05:29:10 +0100

    Add cutom order to contacts

--- src/pages/contact-us.tsx
+++ src/pages/contact-us.tsx
@@ -5,7 +5,7 @@ import ContactCard from '../components/contact-us-page/contact-card';
 
 const ContactList = (props: { contacts: ContactNode[] }) => (
     <div className={'grid grid-cols-1 sm:grid-cols-2 gap-x-4 gap-y-4'}>
-        {sortBy(props.contacts, ['Role', 'Country', 'Name']).map(
+        {sortBy(props.contacts, ['Order', 'Role', 'Country', 'Name']).map(
             (contact: ContactNode, i: number) => (
                 <ContactCard key={i} contact={contact} />
             )
@@ -20,21 +20,22 @@ export default function Page() {
     return (
         <>
             <h2 className='mx-auto mb-6 text-gray-800 md:mb-8 w-60 md:w-auto'>
-                Country Hosts
-            </h2>
-            {countryHosts.length > 0 && <ContactList contacts={countryHosts} />}
-            {countryHosts.length === 0 && (
-                <div className='text-lg italic text-gray-800 font-weight-500'>
-                    No country hosts yet
-                </div>
-            )}
-            <h2 className='mx-auto mt-12 mb-6 text-gray-800 md:mb-8 w-60 md:w-auto'>
                 Contact Us
             </h2>
             {contacts.length > 0 && <ContactList contacts={contacts} />}
             {contacts.length === 0 && (
                 <div className='text-lg italic text-gray-800 font-weight-500'>
                     No contacts yet
+                </div>
+            )}
+            <h2 className='mx-auto mt-12 mb-6 text-gray-800 md:mb-8 w-60 md:w-auto'>
+                Country Hosts
+            </h2>
+
+            {countryHosts.length > 0 && <ContactList contacts={countryHosts} />}
+            {countryHosts.length === 0 && (
+                <div className='text-lg italic text-gray-800 font-weight-500'>
+                    No country hosts yet
                 </div>
             )}
         </>
@@ -45,6 +46,7 @@ const query = graphql`
     query MyQuery {
         allStrapiContact {
             nodes {
+                Order
                 Email
                 Name
                 Role
@@ -55,6 +57,7 @@ const query = graphql`
         }
         allStrapiCountryHost {
             nodes {
+                Order
                 Country
                 Name
                 Email
